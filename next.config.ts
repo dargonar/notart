240d548bd8219c1dee2281c3dfcf1fbfffcbecfc
dargonar <ptutino@gmail.com>
2024-12-20 14:45:12 -0300

    Update next build config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    target: 'server', // This is required for Next.js to work correctly with Netlify
+    reactStrictMode: true,
 };
 
 export default nextConfig;
